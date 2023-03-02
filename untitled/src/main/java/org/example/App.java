d023b13b28c964ca629af9ad8948c6622c2e7690
jojeongbin <cjb1009@naver.com>
2023-03-02 13:55:34 +0900

    각 모듈로 기능 분리

--- untitled/src/main/java/org/example/App.java
+++ untitled/src/main/java/org/example/App.java
@@ -3,6 +3,9 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import org.example.controller.SystemController;
+import org.example.wiseSaying.controller.WiseSayingController;
 import org.example.wiseSaying.entity.WiseSaying;
 
 public class App {
@@ -15,39 +18,26 @@ public class App {
     public void run() {
         System.out.println("==명언 앱==");
 
-        long lastWiseSayingId = 0;
-        List<WiseSaying> wiseSayings = new ArrayList<>();
+        SystemController systemController = new SystemController();
+        WiseSayingController wiseSayingController = new WiseSayingController(sc);
 
         while (true) {
             System.out.print("명령)");
             String command = sc.nextLine().trim();
 
             if (command.equals("종료")) {
+
+                systemController.exit();
                 break;
+
             } else if (command.equals("등록")) {
-                long id = lastWiseSayingId + 1;
-                System.out.print("명언 : ");
-                String content = sc.nextLine().trim();
-                System.out.print("작가 : ");
-                String authorName = sc.nextLine().trim();
 
-                WiseSaying wiseSaying = new WiseSaying(id, content, authorName);
-                wiseSayings.add(wiseSaying);
+                wiseSayingController.write();
 
-                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-                lastWiseSayingId = id; // 증가
             } else if (command.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("-".repeat(30));
-
-                for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-                    WiseSaying wiseSaying = wiseSayings.get(i);
-
-                    System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthorName(), wiseSaying.getContent());
-
-
+                wiseSayingController.list();
                 }
             }
         }
     }
-}
+
