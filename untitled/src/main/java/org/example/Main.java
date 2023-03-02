c52358982f10ab852607591a46623083fbf313a9
jojeongbin <cjb1009@naver.com>
2023-03-02 09:26:44 +0900

    1단계 완성

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -5,7 +5,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-      
+        String command;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("==명언 앱==");
+        System.out.print("명령)");
+        command=sc.next();
+
+
+
     
     }
 }
