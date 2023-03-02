3face55fb2c645c8bfc5dee703f1246fbd3f684d
jojeongbin <cjb1009@naver.com>
2023-03-02 13:17:47 +0900

    1단계 구현 수정 - class 분리

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -5,11 +5,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String command;
         Scanner sc=new Scanner(System.in);
-        System.out.println("==명언 앱==");
-        System.out.print("명령)");
-        command=sc.next();
-        
+        new App(sc).run();
+        sc.close();
     }
 }
